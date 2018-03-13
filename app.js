877b93e1cb995bc1fcdc8c8aab1870b47b3cb3ef
Fábio Nascimento <fbn4sc@gmail.com>
2018-03-13 10:28:19 -0300

    Adds database connection

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const vstsClient = require("./vstsAPIClient");
 
+const mongoose = require("mongoose");
+mongoose.connect(process.env.mongoConnectionString);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Connected to the db");
+});
+
 app.get("/", (req, res) => {
   res.send("Delta API.");
 });
